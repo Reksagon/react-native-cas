fe071b104beeb1a0e33ae98019e096f2f11e1d6a
Denys Korniienko <den.kornienko2012@gmail.com>
2025-08-26 11:11:36 +0300

    add files

---
+++ src/native/Commands.ts
@@ -0,0 +1,60 @@
+import { UIManager, findNodeHandle } from 'react-native';
+
+export const AdViewCommands = {
+  loadAd: (ref: any) => {
+    const reactTag = findNodeHandle(ref);
+    if (!reactTag) return;
+
+    UIManager.dispatchViewManagerCommand(
+      reactTag,
+      UIManager.getViewManagerConfig('AdView').Commands.loadAd,
+      []
+    );
+  },
+
+  destroy: (ref: any) => {
+    const reactTag = findNodeHandle(ref);
+    if (!reactTag) return;
+
+    UIManager.dispatchViewManagerCommand(
+      reactTag,
+      UIManager.getViewManagerConfig('AdView').Commands.destroy,
+      []
+    );
+  },
+
+  startAutoRefresh: (ref: any) => {
+    const reactTag = findNodeHandle(ref);
+    if (!reactTag) return;
+
+    UIManager.dispatchViewManagerCommand(
+      reactTag,
+      UIManager.getViewManagerConfig('AdView').Commands.startAutoRefresh,
+      []
+    );
+  },
+
+  stopAutoRefresh: (ref: any) => {
+    const reactTag = findNodeHandle(ref);
+    if (!reactTag) return;
+
+    UIManager.dispatchViewManagerCommand(
+      reactTag,
+      UIManager.getViewManagerConfig('AdView').Commands.stopAutoRefresh,
+      []
+    );
+  },
+
+  isAdLoaded: (ref: any): Promise<boolean> => {
+    const reactTag = findNodeHandle(ref);
+    if (!reactTag) return Promise.resolve(false);
+
+    return new Promise((resolve) => {
+      UIManager.dispatchViewManagerCommand(
+        reactTag,
+        UIManager.getViewManagerConfig('AdView').Commands.isAdLoaded,
+        [resolve]
+      );
+    });
+  },
+};
---
+++ src/types/Types.ts
@@ -0,0 +1,134 @@
+import type { StyleProp, ViewStyle } from 'react-native';
+
+export type ConsentFlowParams = {
+  enabled?: boolean;
+  privacyPolicy?: string;
+  requestGDPR?: boolean;
+  requestATT?: boolean;
+};
+
+export type MediationExtraParams = {
+  key: string;
+  value: string;
+};
+
+export enum AdType {
+  Banner = 0,
+  Interstitial,
+  Rewarded,
+  AppOpen,
+  Native,
+  None,
+}
+
+export enum Gender {
+  Unknown = 0,
+  Male,
+  Female,
+}
+
+export enum Audience {
+  Undefined = 0,
+  Children,
+  NotChildren,
+}
+
+export type BuildManagerParams = {
+  casId?: string;
+  consentFlow?: ConsentFlowParams;
+  testMode?: boolean;
+  mediationExtra?: MediationExtraParams;
+};
+
+export type CASSettings = {
+  taggedAudience: Audience;
+  age: number;
+  gender: Gender;
+  contentUrl?: string;
+  keywords: Array<string>;
+  debugMode: boolean;
+  mutedAdSounds: boolean;
+  testDeviceIDs: Array<string>;
+  locationCollectionEnabled?: boolean;
+};
+
+export type DismissConsentFlowEvent = {
+  status: number;
+};
+
+export type InitConfiguration = {
+  error?: string;
+  countryCode?: string;
+  isConsentRequired: boolean;
+  consentFlowStatus: number;
+};
+
+export enum AdViewSize {
+  B = 'BANNER',
+  L = 'LEADERBOARD',
+  M = 'MEDIUM_RECTANGLE',
+  A = 'ADAPTIVE',
+  S = 'SMART',
+}
+
+export type AdInfoEvent = Readonly<{
+  adUnitId?: string;
+  adFormat?: string;
+  adViewId?: number;
+  networkName?: string;
+  networkPlacement?: string;
+  creativeId?: string | null;
+  placement?: string | null;
+  revenue?: number;
+  revenuePrecision?: string;
+  latencyMillis?: number;
+  dspName?: string | null;
+  size?: Readonly<{
+    width: number;
+    height: number;
+  }>;
+}>;
+
+export type AdLoadFailedEvent = Readonly<{
+  adUnitId?: string;
+  adViewId?: number;
+  code: number;
+  message?: string | null;
+  mediatedNetworkErrorCode?: number;
+  mediatedNetworkErrorMessage?: string;
+  adLoadFailureInfo?: string | null;
+}>;
+
+export type AdImpression = {
+  adType: AdType;
+  cpm: number;
+  error?: string;
+  identifier: string;
+  impressionDepth: number;
+  lifetimeRevenue: number;
+  network: string;
+  priceAccuracy: number;
+  status: string;
+  versionInfo: string;
+  creativeIdentifier?: string;
+};
+
+export type AdViewPresentedEvent = Readonly<{
+  impression: AdImpression;
+}>;
+
+export type AdViewProps = {
+  style?: StyleProp<ViewStyle>;
+  size: AdViewSize;
+  isAutoloadEnabled?: boolean;
+  refreshInterval?: number;
+  onAdViewLoaded?: () => void;
+  onAdViewFailed?: (e: AdLoadFailedEvent) => void;
+  onAdViewClicked?: () => void;
+  onAdViewImpression?: (e: AdViewPresentedEvent) => void;
+};
+
+export type AdViewRef = {
+  loadAd: () => Promise<void>;
+  isAdLoaded: () => Promise<boolean>;
+};
